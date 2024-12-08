73b2d81c6e2c2cf8a0e1ef061c58616d46fc1597
AbuZobayed <zobayed.ict05@gmail.com>
2024-12-08 23:57:35 +0600

    first commit

---
+++ src/components/Categories/CategoryBox.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types'
+import queryString from 'query-string';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+
+
+
+
+
+const CategoryBox = ({ label, icon: Icon }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [params,setParams] = useSearchParams()
+  const category = params.get('category')
+  console.log();
+  
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+
+    // 1. Create Query String
+    let currentQuery = {category : label}
+    const url = queryString.stringifyUrl({
+      url: '/',
+      query: currentQuery,
+    })
+    // url output ---> /?category=label("?category=label" this is not a route)
+
+    // 2. set query string in url 
+    navigate(url)
+  }
+
+
+  return (
+    <div
+    onClick={handleClick}
+      className={`flex 
+  flex-col 
+  items-center 
+  justify-center 
+  gap-2
+  p-3
+  border-b-2
+  hover:text-neutral-800
+  transition
+  cursor-pointer ${category === label && "border-b-neutral-800 text-neutral-800"}`}
+    >
+      <Icon size={26} />
+      <div className='text-sm font-medium'>{label}</div>
+    </div>
+  )
+}
+
+CategoryBox.propTypes = {
+  label: PropTypes.string,
+  icon: PropTypes.elementType,
+}
+
+export default CategoryBox
