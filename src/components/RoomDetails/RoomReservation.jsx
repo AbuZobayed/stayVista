eb2e9479df711cdc508da0f3c9fd5520b278bc7f
AbuZobayed <zobayed.ict05@gmail.com>
2024-12-25 14:26:31 +0600

    save user in db

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import Button from "../Shared/Button/Button";
 import { useState } from "react";
 import { DateRange } from "react-date-range";
+import { differenceInCalendarDays } from "date-fns";
 
 const RoomReservation = ({ room }) => {
   console.log("start date-->", new Date(room.from).toLocaleDateString());
@@ -15,11 +16,25 @@ const RoomReservation = ({ room }) => {
     },
   ]);
 
+  // total Days * price
+  const totalPrice =
+    parseInt(
+      differenceInCalendarDays(new Date(room.to), new Date(room.from)) 
+    ) * room?.price;
+  /**
+   * const totalPrice =
+    parseInt(
+      differenceInCalendarDays(new Date(room.to), new Date(room.from)) + 1
+    ) * room?.price;
+   */
+
+  console.log(totalPrice);
+
   return (
     <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
       <div className="flex items-center gap-1 p-4">
         <div className="text-2xl font-semibold">$ {room?.price}</div>
-        <div className="font-light text-neutral-600">night</div>
+        <div className="font-light text-neutral-600">/night</div>
       </div>
       <hr />
       <div className="flex justify-center">
@@ -28,19 +43,17 @@ const RoomReservation = ({ room }) => {
           showDateDisplay={false}
           rangeColors={["#F54B68"]}
           // editableDateInputs={true}
-          onChange={(item) =>
-          {
-            console.log(item)
-            
+          onChange={(item) => {
+            console.log(item);
+
             setState([
               {
                 startDate: new Date(room.from),
                 endDate: new Date(room.to),
                 key: "selection",
               },
-            ])
-          }
-          }
+            ]);
+          }}
           moveRangeOnFirstSelection={false}
           ranges={state}
         />
@@ -52,7 +65,7 @@ const RoomReservation = ({ room }) => {
       <hr />
       <div className="p-4 flex items-center justify-between font-semibold text-lg">
         <div>Total</div>
-        <div>${room?.price}</div>
+        <div>${totalPrice}</div>
       </div>
     </div>
   );
